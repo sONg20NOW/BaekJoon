ca68208e90cbe053f3c5fac893e70df5b6c9e929
Kang Gyu Song <37824301+sONg20NOW@users.noreply.github.com>
2025-07-07 17:51:41 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9596 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -8,14 +8,23 @@ const s = input[1];
 const r = 31;
 const M = 1234567891;
 
+function Pow(r, i) {
+    let sigma = 1;
+    for (let j = 0; j < i; j++) {
+        sigma = (sigma * r) % M;
+    }
+    return sigma;
+}
+
 function CalcH(s, L) {
     let sum = 0
 
     for (let i = 0; i < L; i++) {
-        sum += (s[i].charCodeAt() - 'a'.charCodeAt() + 1) * Math.pow(r, i);
+        sum += (s[i].charCodeAt() - 'a'.charCodeAt() + 1) * Pow(r, i);
+        sum = sum % M;
     }
 
-    return sum % M;
+    return sum;
 }
 
 console.log(CalcH(s, L));
