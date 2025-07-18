8801598775a7c69f793691dd2b632afb417c01ab
Kang Gyu Song <37824301+sONg20NOW@users.noreply.github.com>
2025-07-18 14:24:06 +0900

    [Silver III] Title: 주유소, Time: 312 ms, Memory: 42684 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -6,15 +6,15 @@ const n = parseInt(input[0]);
 const lines = input[1].split(' ').map(Number);
 let cities = input[2].split(' ').map(Number);
 // 다음 도시가 마지막 도시라면 그 도시의 주유비는 싸다고 책정. (그 도시까지 그냥 가도록.)
-cities[cities.length - 1] = 0;
+cities[cities.length - 1] = 0n;
 
-let money = 0;
+let money = 0n;
 
-let head = 0;
-let tail = 0;
+let head = 0n;
+let tail = 0n;
 while (tail < cities.length) {
-    const head_fee = cities[head];
-    const tail_fee = cities[tail];
+    const head_fee = BigInt(cities[head]);
+    const tail_fee = BigInt(cities[tail]);
     // 다음 도시의 주유비가 더 싸다면
     if (head_fee > tail_fee) {
         money += sum(head, tail, lines) * head_fee;
@@ -26,12 +26,12 @@ while (tail < cities.length) {
     }
 }
 
-console.log(money);
+console.log(money.toString());
 
 function sum(start, end, arr) {
-    let sum = 0;
+    let sum = 0n;
     for (let i = start; i < end; i++) {
-        sum += arr[i];
+        sum += BigInt(arr[i]);
     }
 
     return sum;
